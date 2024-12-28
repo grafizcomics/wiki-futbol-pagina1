975a5481488bad49e9aed09e919f5e5043a927cc
Grafiz Comics <grafizcomic@gmail.com>
2024-12-28 14:49:18 -0500

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,273 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  var customModal1 = document.getElementById("customModal1");
+  var customImg1 = document.getElementById("customImg1");
+  var customModalImg1 = document.getElementById("customModalImg1");
+  var customCaption1 = document.getElementById("customCaption1");
+  customImg1.onclick = function() {
+    customModal1.style.display = "block";
+    customModalImg1.src = this.src;
+    customCaption1.innerHTML = this.alt;
+  };
+  var closeCustomModal1 = document.getElementsByClassName("close1")[0];
+  closeCustomModal1.onclick = function() {
+    customModal1.style.display = "none";
+  };
+  window.onclick = function(event) {
+    if (event.target == customModal1) {
+      customModal1.style.display = "none";
+    }
+  };
+
+  /////////////////////////////////////////////////////////////////////////////////////////////
+
+  var customModal2 = document.getElementById("customModal2");
+  var customImg2 = document.getElementById("customImg2");
+  var customModalImg2 = document.getElementById("customModalImg2");
+  var customCaption2 = document.getElementById("customCaption2");
+  customImg2.onclick = function() {
+    customModal2.style.display = "block";
+    customModalImg2.src = this.src;
+    customCaption2.innerHTML = this.alt;
+  };
+  var closeCustomModal2 = document.getElementsByClassName("close2")[0];
+  closeCustomModal2.onclick = function() {
+    customModal2.style.display = "none";
+  };
+  window.onclick = function(event) {
+    if (event.target == customModal2) {
+      customModal2.style.display = "none";
+    }
+  };
+
+  /////////////////////////////////////////////////////////////////////////////////////////////
+
+  var customModal01 = document.getElementById("customModal01");
+  var customImg01 = document.getElementById("customImg01");
+  var customModalImg01 = document.getElementById("customModalImg01");
+  var customCaption01 = document.getElementById("customCaption01");
+  customImg01.onclick = function() {
+    customModal01.style.display = "block";
+    customModalImg01.src = this.src;
+    customCaption01.innerHTML = this.alt;
+  };
+  var closeCustomModal01 = document.getElementsByClassName("close01")[0];
+  closeCustomModal01.onclick = function() {
+    customModal01.style.display = "none";
+  };
+  window.onclick = function(event) {
+    if (event.target == customModal01) {
+      customModal01.style.display = "none";
+    }
+  };
+
+  /////////////////////////////////////////////////////////////////////////////////////////////
+
+  var customModal4 = document.getElementById("customModal4");
+  var customImg4 = document.getElementById("customImg4");
+  var customModalImg4 = document.getElementById("customModalImg4");
+  var customCaption4 = document.getElementById("customCaption4");
+  customImg4.onclick = function() {
+    customModal4.style.display = "block";
+    customModalImg4.src = this.src;
+    customCaption4.innerHTML = this.alt;
+  };
+  var closeCustomModal4 = document.getElementsByClassName("close4")[0];
+  closeCustomModal4.onclick = function() {
+    customModal4.style.display = "none";
+  };
+  window.onclick = function(event) {
+    if (event.target == customModal4) {
+      customModal4.style.display = "none";
+    }
+  };
+
+
+  /////////////////////////////////////////////////////////////////////////////////////////////
+
+  var customModal5 = document.getElementById("customModal5");
+  var customImg5 = document.getElementById("customImg5");
+  var customModalImg5 = document.getElementById("customModalImg5");
+  var customCaption5 = document.getElementById("customCaption5");
+  customImg5.onclick = function() {
+    customModal5.style.display = "block";
+    customModalImg5.src = this.src;
+    customCaption5.innerHTML = this.alt;
+  };
+  var closeCustomModal5 = document.getElementsByClassName("close5")[0];
+  closeCustomModal5.onclick = function() {
+    customModal5.style.display = "none";
+  };
+  window.onclick = function(event) {
+    if (event.target == customModal5) {
+      customModal5.style.display = "none";
+    }
+  };
+
+  /////////////////////////////////////////////////////////////////////////////////////////////
+
+  var customModal6 = document.getElementById("customModal6");
+  var customImg6 = document.getElementById("customImg6");
+  var customModalImg6 = document.getElementById("customModalImg6");
+  var customCaption6 = document.getElementById("customCaption6");
+  customImg6.onclick = function() {
+    customModal6.style.display = "block";
+    customModalImg6.src = this.src;
+    customCaption6.innerHTML = this.alt;
+  };
+  var closeCustomModal6 = document.getElementsByClassName("close6")[0];
+  closeCustomModal6.onclick = function() {
+    customModal6.style.display = "none";
+  };
+  window.onclick = function(event) {
+    if (event.target == customModal6) {
+      customModal6.style.display = "none";
+    }
+  };
+
+  /////////////////////////////////////////////////////////////////////////////////////////////
+
+  var customModal7 = document.getElementById("customModal7");
+  var customImg7 = document.getElementById("customImg7");
+  var customModalImg7 = document.getElementById("customModalImg7");
+  var customCaption7 = document.getElementById("customCaption7");
+  customImg7.onclick = function() {
+    customModal7.style.display = "block";
+    customModalImg7.src = this.src;
+    customCaption7.innerHTML = this.alt;
+  };
+  var closeCustomModal7 = document.getElementsByClassName("close7")[0];
+  closeCustomModal7.onclick = function() {
+    customModal7.style.display = "none";
+  };
+  window.onclick = function(event) {
+    if (event.target == customModal7) {
+      customModal7.style.display = "none";
+    }
+  };
+
+  /////////////////////////////////////////////////////////////////////////////////////////////
+
+  var customModal8 = document.getElementById("customModal8");
+  var customImg8 = document.getElementById("customImg8");
+  var customModalImg8 = document.getElementById("customModalImg8");
+  var customCaption8 = document.getElementById("customCaption8");
+  customImg8.onclick = function() {
+    customModal8.style.display = "block";
+    customModalImg8.src = this.src;
+    customCaption8.innerHTML = this.alt;
+  };
+  var closeCustomModal8 = document.getElementsByClassName("close8")[0];
+  closeCustomModal8.onclick = function() {
+    customModal8.style.display = "none";
+  };
+  window.onclick = function(event) {
+    if (event.target == customModal8) {
+      customModal8.style.display = "none";
+    }
+  };
+
+  /////////////////////////////////////////////////////////////////////////////////////////////
+
+  var customModal9 = document.getElementById("customModal9");
+  var customImg9 = document.getElementById("customImg9");
+  var customModalImg9 = document.getElementById("customModalImg9");
+  var customCaption9 = document.getElementById("customCaption9");
+  customImg9.onclick = function() {
+    customModal9.style.display = "block";
+    customModalImg9.src = this.src;
+    customCaption9.innerHTML = this.alt;
+  };
+  var closeCustomModal9 = document.getElementsByClassName("close9")[0];
+  closeCustomModal9.onclick = function() {
+    customModal9.style.display = "none";
+  };
+  window.onclick = function(event) {
+    if (event.target == customModal9) {
+      customModal9.style.display = "none";
+    }
+  };
+
+  /////////////////////////////////////////////////////////////////////////////////////////////
+
+  var customModal10 = document.getElementById("customModal10");
+  var customImg10 = document.getElementById("customImg10");
+  var customModalImg10 = document.getElementById("customModalImg10");
+  var customCaption10 = document.getElementById("customCaption10");
+  customImg10.onclick = function() {
+    customModal10.style.display = "block";
+    customModalImg10.src = this.src;
+    customCaption10.innerHTML = this.alt;
+  };
+  var closeCustomModal10 = document.getElementsByClassName("close10")[0];
+  closeCustomModal10.onclick = function() {
+    customModal10.style.display = "none";
+  };
+  window.onclick = function(event) {
+    if (event.target == customModal10) {
+      customModal10.style.display = "none";
+    }
+  };
+
+
+
+
+
+
+
+});
+
+
+
+
+
+//////////////////////////// ACORDEON
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+////////////   CAMBIAR IMAGEN ///////////
+
+// Cambiar imagen
+function cambiarImagen2(rutaImagen) {
+  const imagenPrincipal2 = document.getElementById('customImg2');
+  imagenPrincipal2.src = rutaImagen;
+}
+// Cambiar imagen
+function cambiarImagen01(rutaImagen) {
+  const imagenPrincipal01 = document.getElementById('customImg01');
+  imagenPrincipal01.src = rutaImagen;
+}
+
+////////////////   DATATABLE 1   //////////
+
+document.addEventListener('DOMContentLoaded', function() {
+  const searchInput = document.getElementById('searchInput');
+  const table = document.getElementById('dataTable');
+  const rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+
+  searchInput.addEventListener('keyup', function() {
+      const filter = searchInput.value.toLowerCase();
+      for (let i = 0; i < rows.length; i++) {
+          let cells = rows[i].getElementsByTagName('td');
+          let rowText = Array.from(cells).map(cell => cell.textContent.toLowerCase()).join(' ');
+          rows[i].style.display = rowText.includes(filter) ? '' : 'none';
+      }
+  });
+});
